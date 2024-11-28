57aed9e9a21824f712e95882595f877702c06487
cris28314 <08csanchez@institutperevives.cat>
2024-11-28 12:50:52 +0100

    document projecte

---
+++ projecte.py
@@ -0,0 +1,47 @@
+agenda_de_contactes = []
+
+contactes1 = {
+    'Nom':'Joan',
+    'Telefon': '612345678',
+    'Gmail':'joan@example.com',
+}
+
+contactes2 = {
+    'Nom':'Maria',
+    'Telefon':'698765432',
+    'Gmail':'maria@example.com',
+}
+contactes3 = {
+    'Nom':'Pere',
+    'Telefon':'678123456',
+    'Gmail':'pere@example.com',
+}
+
+agenda_de_contactes.append(contactes1)
+
+agenda_de_contactes.append(contactes2)
+
+agenda_de_contactes.append(contactes3)
+
+print (agenda_de_contactes)
+
+
+print('Benbingut a la app de la gestió de contactes. Que vols fer?')
+print('Opció 1: Afegir un element a la agenda.')
+print('Opció 2: Eliminar un element de la agenda.')
+print('Opció 3: Mostrar la agenda.')
+
+
+for contacte_nou in agenda_de_contactes:
+    
+
+#nom = input('Nom del contacte: ')
+#telefon = input('Numero de telefon: ')
+#gmail = input('Correu electronic: ')
+
+#contacte = {'Nom':nom,'Telefon':telefon,'Gmail':gmail}
+
+#agenda_de_contactes.append(contacte)
+
+#agenda_de_contactes[nom] = contacte
+
